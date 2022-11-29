10492592f0478a69ab01808eadc34822f7f21632
Huntney1 <devilwhiteone@hotmail.it>
2022-11-29 17:47:27 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -4,17 +4,18 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
         console.log('FizzBazz')
-        square = `<div class="square square-coral">FizzBuzz</div>`
+        square = `<div class="square square-lime">FizzBuzz</div>`
     }
     else if (i % 3 == 0) {
         console.log('Fizz')
-        square = `<div class="square square-dodgerblue">Fizz</div>`
+        square = `<div class="square square-violet">Fizz</div>`
     }
     else if (i % 5 == 0) {
         console.log('Bizz')
-        square = `<div class="square square-violet">Bazz</div>`
+        square = `<div class="square square-coral">Bazz</div>`
     }
     else {
+        square = `<div class="square square-dodgerblue">${i}</div>`
         console.log(i)
     }
     container.innerHTML += square;
