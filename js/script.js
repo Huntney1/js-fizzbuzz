8a04b1b8df867dd921207a05848760c64dcc4477
Huntney1 <devilwhiteone@hotmail.it>
2022-11-29 16:46:55 +0100

    fix html  css e js

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@ for (let i = 1; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
         console.log('FizzBazz')
+        square = `<div class="square square-coral">FizzBuzz</div>`
     }
     else if (i % 3 == 0) {
         console.log('Fizz')
-
+        square = `<div class="square square-dodgerblue">Fizz</div>`
     }
     else if (i % 5 == 0) {
         console.log('Bizz')
-
+        square = `<div class="square square-violet">Bazz</div>`
     }
-    else{
+    else {
         console.log(i)
     }
-   
+
 }
